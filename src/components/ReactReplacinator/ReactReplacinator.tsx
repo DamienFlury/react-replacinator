0f8f67d996a734c55898e75d0e61617cb62b96e8
Aaron <aaron@stampa.ch>
2020-06-24 09:35:11 +0200

    Extract editor to parent, add communincation with TagBar

--- src/components/ReactReplacinator/ReactReplacinator.tsx
+++ src/components/ReactReplacinator/ReactReplacinator.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { Node } from "slate";
+import React, { useState, useMemo } from "react";
+import { Node, createEditor } from "slate";
 import TagBar from "./TagBar";
 import { Tag } from "./TagBar/types";
 import TemplateEditor from "./TemplateEditor";
+import { withReact } from "slate-react";
 
 const tags: Tag[] = [
   {
@@ -24,15 +25,27 @@ const ReactReplacinator: React.FC = () => {
       children: [{ text: "This is a paragraph" }],
     },
   ]);
+
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  const insertPlaceholder = (tag: Tag) => {
+    editor.insertNode({
+      type: "placeholder",
+      children: [{ text: "" }],
+      data: { color: tag.backgroundColor, text: tag.name },
+    });
+    editor.insertText("");
+  };
+
   return (
     <>
       <TagBar
         tags={tags}
         onSelect={(tag) => {
-          console.log(tag.name);
+          insertPlaceholder(tag);
         }}
       />
-      <TemplateEditor value={value} setValue={setValue} />
+      <TemplateEditor value={value} setValue={setValue} editor={editor} />
     </>
   );
 };
--- src/components/ReactReplacinator/TemplateEditor.tsx
+++ src/components/ReactReplacinator/TemplateEditor.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo, useCallback } from "react";
-import { Slate, Editable, withReact, RenderElementProps } from "slate-react";
-import { createEditor, Node } from "slate";
+import React, { useCallback } from "react";
+import { Slate, Editable, RenderElementProps, ReactEditor } from "slate-react";
+import { Node, Editor } from "slate";
 import "./TemplateEditor.css";
 
 const DefaultElement: React.FC<RenderElementProps> = (props) => {
@@ -27,11 +27,10 @@ const PlaceholderElement: React.FC<RenderElementProps> = (props) => {
 type Props = {
   value: Node[];
   setValue: React.Dispatch<React.SetStateAction<Node[]>>;
+  editor: Editor & ReactEditor;
 };
 
-const TemplateEditor: React.FC<Props> = ({ value, setValue }) => {
-  const editor = useMemo(() => withReact(createEditor()), []);
-
+const TemplateEditor: React.FC<Props> = ({ value, setValue, editor }) => {
   editor.isInline = (element) => {
     return element.type === "placeholder";
   };
@@ -47,23 +46,6 @@ const TemplateEditor: React.FC<Props> = ({ value, setValue }) => {
   }, []);
   return (
     <Slate editor={editor} value={value} onChange={setValue}>
-      <button
-        type="button"
-        onMouseDown={(event) => {
-          event.preventDefault();
-          editor.insertNode({
-            type: "placeholder",
-            children: [{ text: "" }],
-            data: { color: "#aaffaa", text: "First Name" },
-          });
-          editor.insertNode({
-            type: "paragraph",
-            children: [{ text: "" }],
-          });
-        }}
-      >
-        First Name
-      </button>
       <Editable renderElement={renderElement} />
     </Slate>
   );
