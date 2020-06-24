b2af692d1a1122a067810e53d2822ae385f5b531
Damien Flury <damien.flury@gmail.com>
2020-06-24 09:37:33 +0200

    :sparkles: Move tags to props

--- src/components/ReactReplacinator/ReactReplacinator.tsx
+++ src/components/ReactReplacinator/ReactReplacinator.tsx
@@ -4,24 +4,15 @@ import TagBar from "./TagBar";
 import { Tag } from "./TagBar/types";
 import TemplateEditor from "./TemplateEditor";
 
-const tags: Tag[] = [
-  {
-    name: "First Name",
-    backgroundColor: "#23aaff",
-    color: "#ffffff",
-  },
-  {
-    name: "Last Name",
-    backgroundColor: "#ff55aa",
-    color: "#ffffff",
-  },
-];
+type Props = {
+  tags: Tag[];
+};
 
-const ReactReplacinator: React.FC = () => {
+const ReactReplacinator: React.FC<Props> = ({ tags }) => {
   const [value, setValue] = useState<Node[]>([
     {
       type: "paragraph",
-      children: [{ text: "This is a paragraph" }],
+      children: [{ text: "" }],
     },
   ]);
   return (
