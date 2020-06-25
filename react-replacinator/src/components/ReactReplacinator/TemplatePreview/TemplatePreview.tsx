f203e89fca47f34112ab2784b6fc2aefa222edc5
Damien Flury <damien.flury@gmail.com>
2020-06-25 11:01:36 +0200

    :rotating_light: Fix linter errors

--- react-replacinator/src/components/ReactReplacinator/TemplatePreview/TemplatePreview.tsx
+++ react-replacinator/src/components/ReactReplacinator/TemplatePreview/TemplatePreview.tsx
@@ -12,26 +12,27 @@ export type ChildProps = {
 };
 
 const TemplatePreview: React.FC<Props> = ({ nodes }) => {
-  const renderNodes = () =>
-    nodes.map((node, index) => (
-      <React.Fragment key={index}>
-        {(node.children as (Node & ChildProps)[]).map((child, childIndex) => {
-          if (child.type === "placeholder") {
-            return (
-              <span
-                key={childIndex}
-                style={{ fontStyle: "italic", fontWeight: "bold" }}
-              >
-                {child.data.text}
-              </span>
-            );
-          }
-          return child.text;
-        })}
-        <br />
-      </React.Fragment>
-    ));
-  return <div style={{ whiteSpace: "pre-wrap" }}>{renderNodes()}</div>;
+  const preview = nodes.map((node, index) => (
+    // eslint-disable-next-line react/no-array-index-key
+    <React.Fragment key={index}>
+      {(node.children as (Node & ChildProps)[]).map((child, childIndex) => {
+        if (child.type === "placeholder") {
+          return (
+            <span
+              // eslint-disable-next-line react/no-array-index-key
+              key={childIndex}
+              style={{ fontStyle: "italic", fontWeight: "bold" }}
+            >
+              {child.data.text}
+            </span>
+          );
+        }
+        return child.text;
+      })}
+      <br />
+    </React.Fragment>
+  ));
+  return <div style={{ whiteSpace: "pre-wrap" }}>{preview}</div>;
 };
 
 export default TemplatePreview;
